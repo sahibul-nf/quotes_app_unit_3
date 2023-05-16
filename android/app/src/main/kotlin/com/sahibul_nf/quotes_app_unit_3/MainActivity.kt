6bf64acc720b5ec1fd5b22794670c1090660ce43
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-05-16 18:38:12 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/sahibul_nf/quotes_app_unit_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sahibul_nf.quotes_app_unit_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'views/menu.dart';
+import 'views/themes/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  String supabaseUrl = dotenv.get('SUPABASE_URL');
+  String supabaseAnonKey = dotenv.get('SUPABASE_ANON_KEY');
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quotes App',
+      theme: ThemeData(
+        primarySwatch: MaterialColor(
+          0xffFEF0F1,
+          <int, Color>{
+            50: MyColors.secondary.withOpacity(0.1),
+            100: MyColors.secondary.withOpacity(0.2),
+            200: MyColors.secondary.withOpacity(0.3),
+            300: MyColors.secondary.withOpacity(0.4),
+            400: MyColors.secondary.withOpacity(0.5),
+            500: MyColors.secondary.withOpacity(0.6),
+            600: MyColors.secondary.withOpacity(0.7),
+            700: MyColors.secondary.withOpacity(0.8),
+            800: MyColors.secondary.withOpacity(0.9),
+            900: MyColors.secondary.withOpacity(1.0),
+          },
+        ),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const Menu(),
+    );
+  }
+}
