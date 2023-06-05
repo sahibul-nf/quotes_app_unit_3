53a47c0a678f6597bc5d92dc01fa14bd245b3393
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-06-05 23:40:08 +0700

    integrate API getRandomQuotes

--- lib/controllers/quotes_controller.dart
+++ lib/controllers/quotes_controller.dart
@@ -4,24 +4,28 @@ import 'package:quotes_app_unit_3/models/quote_api_model.dart';
 import '../repositories/quotes_repository.dart';
 
 final getRandomQuotesProvider = FutureProvider<List<QuoteApi>>((ref) async {
-  // panggil repository
-  final repository = ref.watch(quotesRepositoryProvider);
+  // panggil class repository via provider
+  final repo = ref.watch(quotesRepositoryProvider);
 
-  // panggil fungsi get random quotes
-  final res = await repository.getRandomQuotes();
+  // panggil class controller
+  final controller = QuotesController(repo);
 
-  // return hasil ke UI
-  return res;
+  // dapatin data dari fungsi getRandomQuotes()
+  final dataRandomQuotes = await controller.getRandomQuotes();
+
+  // return data ke UI
+  return dataRandomQuotes;
 });
 
-// class QuotesController {
-//   final WidgetRef ref;
-//   QuotesController(this.ref);
+class QuotesController {
+  QuotesController(this._quotesRepository);
 
-//   // get random quotes from repository
-//   Future<List<QuoteApi>> getRandomQuotes() async {
-//     final res = await ref.watch(quotesRepositoryProvider).getRandomQuotes();
+  final QuotesRepository _quotesRepository;
 
-//     return res;
-//   }
-// }
+  // get random quotes from repository
+  Future<List<QuoteApi>> getRandomQuotes() async {
+    final res = await _quotesRepository.getRandomQuotes();
+
+    return res;
+  }
+}
--- lib/views/pages/quotes_page.dart
+++ lib/views/pages/quotes_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:quotes_app_unit_3/controllers/quotes_controller.dart';
+import 'package:quotes_app_unit_3/views/themes/colors.dart';
 
-import '../pages/quote_detail_page.dart';
 import '../themes/typography.dart';
 import '../widgets/icon_solid_light.dart';
 import '../widgets/quotes_card.dart';
+import 'quote_detail_page.dart';
 import 'search_page.dart';
 
-class QuotesPage extends StatelessWidget {
+class QuotesPage extends ConsumerWidget {
   const QuotesPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final quotesState = ref.watch(getRandomQuotesProvider);
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -51,43 +56,66 @@ class QuotesPage extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    right: 16,
-                    left: 16,
-                    bottom: 16,
-                    top: index == 0 ? 100 : 0,
-                  ),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const QuoteDetailPage(
-                            content:
-                                "The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing.",
-                            author: "Rick Riordan",
-                            authorAvatar: "assets/img_avatar.png",
-                            authorJob: "Co-Founder of The Walt Disney Company",
-                          ),
+
+          // nampilin data random quotes
+          quotesState.when(
+            data: (quotes) {
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                        right: 16,
+                        left: 16,
+                        bottom: 16,
+                        top: index == 0 ? 100 : 0,
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const QuoteDetailPage(
+                                content:
+                                    "The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing. The best way to get started is to quit talking and begin doing.",
+                                author: "Rick Riordan",
+                                authorAvatar: "assets/img_avatar.png",
+                                authorJob:
+                                    "Co-Founder of The Walt Disney Company",
+                              ),
+                            ),
+                          );
+                        },
+                        child: QuotesCard(
+                          author: quotes[index].author!,
+                          authorAvatar: "assets/img_avatar.png",
+                          authorJob: "Co-Founder of The Walt Disney Company",
+                          content: quotes[index].content!,
                         ),
-                      );
-                    },
-                    child: const QuotesCard(
-                      author: "Rick Riordan",
-                      authorAvatar: "assets/img_avatar.png",
-                      authorJob: "Co-Founder of The Walt Disney Company",
-                      content:
-                          "The best way to get started is to quit talking and begin doing.",
+                      ),
+                    );
+                  },
+                  childCount: quotes.length,
+                ),
+              );
+            },
+            error: (error, _) {
+              return SliverToBoxAdapter(
+                child: Text(error.toString()),
+              );
+            },
+            loading: () {
+              return SliverToBoxAdapter(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: MyColors.primary,
                     ),
                   ),
-                );
-              },
-              childCount: 7,
-            ),
-          )
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
